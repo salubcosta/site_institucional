07fed8a87026a768dec11d96be9715d6b799f9c4
salubcosta <salubcosta@gmail.com>
2016-12-21 15:33:10 -0300

    pequena mudança pra visualizar como ficará a pequena descrição do site

--- views/home.php
+++ views/home.php
@@ -1,4 +1,5 @@
-<?php
-/*aqui ficará o modelo da home*/
-echo 'Model - home';
-?>
+<div class="home_sobre">
+  <img src="" width="150" height="150"/>
+  <h4>Sobre</h4>
+  <p>Nesta parte fica uma pequena descrição sobre a instituição</p>
+</div>
