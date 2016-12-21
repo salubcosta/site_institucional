cffa095d710194cab6f4a87b03f553fb0a29c81c
salubcosta <salubcosta@gmail.com>
2016-12-21 15:31:53 -0300

    Organizando mais ou menos o esqueleto do site

--- views/template.php
+++ views/template.php
@@ -2,14 +2,34 @@
 <html lang="pt-br">
 <head>
 	<meta charset="uft-8">
-	<title>Página modelo para todo o Projeto</title>
+	<title>Site Institucional</title>
+	<link rel="stylesheet" href="./assets/css/template.css" />
 </head>
 <body>
+	<!--topo-->
+	<div class="topo">
+		<div class="topo1"></div>
+		<div class="banner"></div>
+	</div>
+	<!--fim topo-->
 
-<?php 
+	<!--menu-->
+	<div class="menu">
+		<ul>
+			<a href="./"><li>HOME</li></a>
+			<a href="./portfolio"><li>PORTFOLIO</li></a>
+			<a href="./sobre"><li>SOBRE</li></a>
+			<a href="./servicos"><li>SERVIÇOS</li></a>
+			<a href="./contato"><li>CONTATO</li></a>
+		</ul>
+	</div>
+	<!--fim menu-->
+	<!--container onde ficará o conteúdo dinâmico-->
+	<div class="container">
+		<?php	$this->carregarViewNoTemplate($view, $array); 	?>
+ </div>
+ <!--fim container-->
 
-	$this->carregarViewNoTemplate($view, $array);
-
- ?>
+ <div class="rodape"></div>
 </body>
 </html>
