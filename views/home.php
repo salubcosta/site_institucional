5dd0ca9ea435ccc2d87edf2caff45d12133f8a47
Salumão Barbosa da Costa <salubcosta@gmail.com>
2016-12-22 18:19:28 -0300

    atualização na view home. Inclusão de items para aparecer na home

--- views/home.php
+++ views/home.php
@@ -1,5 +1,16 @@
 <div class="home_sobre">
-  <img src="" width="150" height="150"/>
-  <h4>Sobre</h4>
-  <p>Nesta parte fica uma pequena descrição sobre a instituição</p>
+ 	<img src="" width="150" height="150"/>
+ 	<h4>Sobre</h4>
+ 	<p>Nesta parte fica uma pequena descrição sobre a instituição</p>
+</div>
+
+<div class="home_portfolio">
+	<h4>Últimos Trabalhos</h4>
+	<?php foreach($portfolio as $item): ?>
+	<div class="portfolio_item">
+		<img src="./assets/images/<?php echo $item['imagem'] ?>" border="0" width="200" height="150">
+	</div>
+	<?php endforeach; ?>
+
+	<div style="clear: both;"></div> <!--DIV apenas pra não quebrar o layout-->
 </div>
