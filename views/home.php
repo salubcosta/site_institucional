2a2b35d5f511c297bbf6cff68169b565b99c5d17
Salumão Barbosa da Costa <salubcosta@gmail.com>
2016-12-26 00:45:49 -0300

    inclusão de informações da instituição recuperada no db

--- views/home.php
+++ views/home.php
@@ -1,16 +1,16 @@
 <div class="home_sobre">
- 	<img src="" width="150" height="150"/>
+ 	<img src="./assets/images/<?php echo $portfolio[0]['imagem']; ?>"/>
  	<h4>Sobre</h4>
- 	<p>Nesta parte fica uma pequena descrição sobre a instituição</p>
+ 	<p><?php echo $sobre['descricao']; ?></p>
 </div>
 
 <div class="home_portfolio">
 	<h4>Últimos Trabalhos</h4>
+	<hr />
 	<?php foreach($portfolio as $item): ?>
 	<div class="portfolio_item">
-		<img src="./assets/images/<?php echo $item['imagem'] ?>" title="<?php echo $item['descricao'];?>" border="0" width="200" height="150">
+		<img src="./assets/images/<?php echo $item['imagem'] ?>" title="<?php echo $item['descricao'];?>">
 	</div>
 	<?php endforeach; ?>
-
 	<div style="clear: both;"></div> <!--DIV apenas pra não quebrar o layout-->
 </div>
