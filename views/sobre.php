c29a37c5db54b0c5be6330f85bb04bf0ade4e67f
Salumão Barbosa da Costa <salubcosta@gmail.com>
2016-12-26 00:54:09 -0300

    criação da view sobre.php

---
+++ views/sobre.php
@@ -0,0 +1,12 @@
+<div class="espacamento">
+	<h4><?php echo $sobre['nome']; ?></h4>
+
+	<p>
+		<?php echo  $sobre['descricao']; ?>
+	</p>
+	<p>
+		<?php echo $sobre['sobre']; ?>
+	</p>
+	<hr />
+	<?php echo $sobre['email'] ?></small>
+</div>
