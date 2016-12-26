49952597a0561ebfa1e2efa6502b9c13bfb95e0e
Salumão Barbosa da Costa <salubcosta@gmail.com>
2016-12-26 00:44:06 -0300

    resolvendo questão de acentuação uft8

--- core/model.php
+++ core/model.php
@@ -8,6 +8,7 @@ class model{
 		try{
 			$this->db = new PDO('mysql:host='.HOST.';dbname='.DBNAME, DBUSER, DBPASS);
 			$this->db->exec('SET CHARACTER SET uft8');
+			$this->db->exec("SET NAMES 'utf8'");
 			$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 		}catch(PDOException $e){
 			echo "Falha: ". $e->getMessage();
