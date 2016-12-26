aac1aa4157a2afb3543b4246464cbc27070c68b1
Salumão Barbosa da Costa <salubcosta@gmail.com>
2016-12-26 00:47:08 -0300

    atualização

--- views/portfolio.php
+++ views/portfolio.php
@@ -1,5 +1,6 @@
-<div class="portfolio">
+<div class="portfolio espacamento">
 	<h4>Portfolio - Últimos trabalhos</h4>
+	<hr />
 	<?php foreach($portfolio as $item): ?>
 		<img src="./assets/images/<?php echo $item['imagem']; ?>" title="<?php echo $item['descricao'];?>"/>
 	<?php endforeach; ?>
